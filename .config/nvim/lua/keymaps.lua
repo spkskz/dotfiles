2063b9c91e6261f3dabf21786ec267038edb2371
sp <spksk@protonmail.com>
2024-09-11 21:59:55 +0700

    remove window keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,12 +10,6 @@ keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 -- select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
--- switch window
-keymap.set("n", "<leader>sh", "<C-w>h")
-keymap.set("n", "<leader>sk", "<C-w>k")
-keymap.set("n", "<leader>sj", "<C-w>j")
-keymap.set("n", "<leader>sl", "<C-w>l")
-
 -- Diagnostics
 keymap.set("n", "<C-d>", function()
 	vim.diagnostic.goto_next()
